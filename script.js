63ac4ef1a5df0c90a4d088533f35a1cf63d516e4
jay <jay.ashinde1406@gmail.com>
2023-08-25 20:26:38 +0530

    Bubble Game

---
+++ script.js
@@ -0,0 +1,50 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scorevalue").textContent = score;
+}
+
+
+function getNewHit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#Hitvalue").textContent = hitrn;
+}
+
+function makeBubble() {
+    var clutter = "";
+    for (var i = 1; i < 100; i++) {
+        var rn = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runtimer() {
+    var timerinter = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else {
+            clearInterval(timerinter);
+            document.querySelector("#pbtm").innerHTML = `<h1> Game Over<h1>`;
+        }
+
+    }, 1000);
+}
+document.querySelector("#pbtm")
+    .addEventListener("click", function (details) {
+        var clickednum = Number(details.target.textContent);
+        if (clickednum == hitrn) {
+            increaseScore();
+            makeBubble()
+            getNewHit();
+        }
+    })
+
+runtimer();
+makeBubble();
+getNewHit();
